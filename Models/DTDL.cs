132e2f30d889d9bfd68a9f8ba54f68bc21368c15
Erich Barnstedt <erichb@microsoft.com>
2023-04-24 23:30:00 +0200

    Initial DTDL parsing and sample.

---
+++ Models/DTDL.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Opc.Ua.Edge.Translator.Models
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public class DTDL
+    {
+        [JsonProperty("@context")]
+        public string Context { get; set; }
+
+        [JsonProperty("@id")]
+        public string Id { get; set; }
+
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("contents")]
+        public List<Content> Contents { get; set; }
+    }
+
+    public class Content
+    {
+        [JsonProperty("@type")]
+        public string type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("schema")]
+        public string Schema { get; set; }
+    }
+}
