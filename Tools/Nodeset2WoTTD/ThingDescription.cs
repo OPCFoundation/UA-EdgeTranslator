29531ad4e70619a786abb12689c3c997c307c408
Erich Barnstedt <erichb@microsoft.com>
2024-07-30 18:00:14 +0200

    Initial checkin of Nodeset to WoT Thing Model conversion tool and sample test file.

---
+++ Tools/Nodeset2WoTTD/ThingDescription.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Opc.Ua.Edge.Translator.Tools
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+#nullable enable
+
+    public class ThingDescription
+    {
+        [JsonProperty("@context")]
+        public object[]? Context { get; set; }
+
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("securityDefinitions")]
+        public SecurityDefinitions? SecurityDefinitions { get; set; }
+
+        [JsonProperty("security")]
+        public string[]? Security { get; set; }
+
+        [JsonProperty("@type")]
+        public string[]? Type { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("base")]
+        public string? Base { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("properties")]
+        public Dictionary<string, Property>? Properties { get; set; }
+    }
+
+    public class OpcUaNamespaces
+    {
+        [JsonProperty("opcua")]
+        public Uri[]? Namespaces { get; set; }
+    }
+
+    public class Property
+    {
+        [JsonProperty("type")]
+        public TypeEnum Type { get; set; }
+
+        [JsonProperty("opcua:nodeId")]
+        public string? OpcUaNodeId { get; set; }
+
+        [JsonProperty("opcua:type")]
+        public string? OpcUaType { get; set; }
+
+        [JsonProperty("opcua:fieldPath")]
+        public string? OpcUaFieldPath { get; set; }
+
+        [JsonProperty("readOnly")]
+        public bool ReadOnly { get; set; }
+
+        [JsonProperty("observable")]
+        public bool Observable { get; set; }
+
+        [JsonProperty("forms")]
+        public object[]? Forms { get; set; }
+    }
+
+    // see https://github.com/eclipse-thingweb/node-wot/tree/master/packages/binding-modbus
+    public class ModbusForm
+    {
+        [JsonProperty("href")]
+        public string? Href { get; set; }
+
+        [JsonProperty("op")]
+        public Op[]? Op { get; set; }
+
+        [JsonProperty("modv:type")]
+        public ModbusType ModbusType { get; set; }
+
+        [JsonProperty("modv:entity")]
+        public ModbusEntity ModbusEntity { get; set; }
+
+        [JsonProperty("modv:pollingTime")]
+        public long ModbusPollingTime { get; set; }
+    }
+
+    // see https://github.com/eclipse-thingweb/node-wot/tree/master/packages/binding-opcua
+    public class OPCUAForm
+    {
+        [JsonProperty("href")]
+        public string? Href { get; set; }
+
+        [JsonProperty("op")]
+        public Op[]? Op { get; set; }
+
+        [JsonProperty("opcua:type")]
+        public OPCUAType OPCUAType { get; set; }
+
+        [JsonProperty("opcua:pollingTime")]
+        public long OPCUAPollingTime { get; set; }
+    }
+
+    public class SecurityDefinitions
+    {
+        [JsonProperty("nosec_sc")]
+        public NosecSc? NosecSc { get; set; }
+    }
+
+    public class NosecSc
+    {
+        [JsonProperty("scheme")]
+        public string? Scheme { get; set; }
+    }
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum ModbusEntity
+    {
+        [EnumMember(Value = "HoldingRegister")]
+        HoldingRegister
+    };
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum ModbusType
+    {
+        [EnumMember(Value = "xsd:float")]
+        Float
+    };
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum OPCUAType
+    {
+        [EnumMember(Value = "xsd:float")]
+        Float
+    };
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum Op
+    {
+        [EnumMember(Value = "observeproperty")]
+        Observeproperty,
+
+        [EnumMember(Value = "readproperty")]
+        Readproperty
+    };
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum TypeEnum
+    {
+        [EnumMember(Value = "number")]
+        Number
+    };
+}
