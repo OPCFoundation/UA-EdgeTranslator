94a8cfcbf1344d318c5ea0445670fd5ea69eabe7
Erich Barnstedt <erichb@microsoft.com>
2025-05-09 08:07:02 +0200

    LoRaWAN support initial check-in.

---
+++ ProtocolDrivers/LoRaWANNetworkServer.cs
@@ -0,0 +1,110 @@
+namespace Opc.Ua.Edge.Translator.ProtocolDrivers
+{
+    using LoRaWan.NetworkServer;
+    using LoRaWan.NetworkServer.BasicsStation;
+    using Opc.Ua.Edge.Translator.Interfaces;
+    using Opc.Ua.Edge.Translator.Models;
+    using Serilog;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class LoRaWANNetworkServer : IAsset
+    {
+        private string _endpoint = string.Empty;
+
+        private List<string> _discoverdAssets = new();
+
+        public LoRaWANNetworkServer()
+        {
+            using var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+
+            var configuration = NetworkServerConfiguration.CreateFromEnvironmentVariables();
+            BasicsStationNetworkServer.RunServerAsync(configuration, cancellationToken).GetAwaiter().GetResult();
+        }
+
+        public List<string> Discover()
+        {
+
+            return _discoverdAssets;
+        }
+
+        public ThingDescription BrowseAndGenerateTD(string name, string endpoint)
+        {
+            ThingDescription td = new()
+            {
+                Context = new string[1] { "https://www.w3.org/2022/wot/td/v1.1" },
+                Id = "urn:" + name,
+                SecurityDefinitions = new() { NosecSc = new NosecSc() { Scheme = "nosec" } },
+                Security = new string[1] { "nosec_sc" },
+                Type = new string[1] { "Thing" },
+                Name = name,
+                Base = endpoint,
+                Title = name,
+                Properties = new Dictionary<string, Property>()
+            };
+
+            return td;
+        }
+
+        public void Connect(string ipAddress, int port)
+        {
+            try
+            {
+                var addresses = ipAddress.Split('/');
+                if (addresses.Length == 2)
+                {
+                    _endpoint = addresses[0];
+                    var deviceId = uint.Parse(addresses[1]);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex.Message, ex);
+            }
+        }
+
+        public void Disconnect()
+        {
+            // nothing to do
+        }
+
+        public string GetRemoteEndpoint()
+        {
+            return _endpoint;
+        }
+
+        public Task<byte[]> Read(string addressWithinAsset, byte unitID, string function, ushort count)
+        {
+            try
+            {
+                // TODO
+
+                return Task.FromResult((byte[])null);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex.Message);
+                return Task.FromResult((byte[])null);
+            }
+        }
+
+        public Task Write(string addressWithinAsset, byte unitID, string function, byte[] values, bool singleBitOnly)
+        {
+            try
+            {
+                // TODO
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex.Message);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
