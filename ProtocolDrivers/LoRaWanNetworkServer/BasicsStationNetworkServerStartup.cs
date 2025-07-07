aa2995f3e471134987364de741d97b9e04379bf4
Erich Barnstedt <erichb@microsoft.com>
2025-07-07 16:34:23 +0200

    Remove custom logger.

--- ProtocolDrivers/LoRaWanNetworkServer/BasicsStationNetworkServerStartup.cs
+++ ProtocolDrivers/LoRaWanNetworkServer/BasicsStationNetworkServerStartup.cs
@@ -10,9 +10,6 @@ namespace LoRaWan.NetworkServer.BasicsStation
     using Microsoft.AspNetCore.Server.Kestrel.Https;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-    using System;
-    using System.Globalization;
 
     internal sealed class BasicsStationNetworkServerStartup(IConfiguration configuration)
     {
@@ -22,50 +19,41 @@ namespace LoRaWan.NetworkServer.BasicsStation
 
         public void ConfigureServices(IServiceCollection services)
         {
-            _ = services.AddLogging(loggingBuilder =>
-                {
-                    _ = loggingBuilder.ClearProviders();
-                    var logLevel = int.TryParse(NetworkServerConfiguration.LogLevel, NumberStyles.Integer, CultureInfo.InvariantCulture, out var logLevelNum)
-                        ? (LogLevel)logLevelNum is var level && Enum.IsDefined(level) ? level : throw new InvalidCastException()
-                        : Enum.Parse<LogLevel>(NetworkServerConfiguration.LogLevel, true);
-
-                    _ = loggingBuilder.SetMinimumLevel(logLevel);
-                    _ = loggingBuilder.AddLoRaConsoleLogger(c => c.LogLevel = logLevel);
-                })
-                .AddMemoryCache()
-                .AddSingleton(NetworkServerConfiguration)
-                .AddSingleton<LoRaADRStrategyProvider>()
-                .AddSingleton<LoRAADRManagerFactory>()
-                .AddSingleton<DataMessageHandler>()
-                .AddSingleton<JoinRequestMessageHandler>()
-                .AddSingleton<MessageDispatcher>()
-                .AddSingleton<BasicsStationConfigurationService>()
-                .AddSingleton<DownlinkMessageSender>()
-                .AddSingleton<ConcentratorDeduplication>();
+            services.AddMemoryCache();
+            services.AddSingleton(NetworkServerConfiguration);
+            services.AddSingleton<LoRaADRStrategyProvider>();
+            services.AddSingleton<LoRAADRManagerFactory>();
+            services.AddSingleton<DataMessageHandler>();
+            services.AddSingleton<JoinRequestMessageHandler>();
+            services.AddSingleton<MessageDispatcher>();
+            services.AddSingleton<BasicsStationConfigurationService>();
+            services.AddSingleton<DownlinkMessageSender>();
+            services.AddSingleton<ConcentratorDeduplication>();
 
             if (NetworkServerConfiguration.ClientCertificateMode is not ClientCertificateMode.NoCertificate)
             {
-                _ = services.AddSingleton<ClientCertificateValidatorService>();
+                services.AddSingleton<ClientCertificateValidatorService>();
             }
         }
 
         // Startup class methods should not be static
         public void Configure(IApplicationBuilder app)
         {
-            app.UseRouting()
-               .UseWebSockets()
-               .UseMiddleware<WebsocketJsonMiddlewareLoRaWAN>()
-               .Run(async (context) =>
-               {
-                    if (context.Request.Path.Value == "/")
-                    {
-                        await context.Response.WriteAsync("LoRaWAN Network Server running.").ConfigureAwait(false);
-                    }
-                    else
-                    {
-                        await context.Response.WriteAsync("Invalid Request").ConfigureAwait(false);
-                    }
-               });
+            app.UseRouting();
+
+            app.UseWebSockets();
+            app.UseMiddleware<WebsocketJsonMiddlewareLoRaWAN>();
+
+            app.Run(async (context) => {
+                if (context.Request.Path.Value == "/")
+                {
+                    await context.Response.WriteAsync("LoRaWAN Network Server running.").ConfigureAwait(false);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Invalid Request").ConfigureAwait(false);
+                }
+            });
         }
     }
 }
